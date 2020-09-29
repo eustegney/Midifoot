9bee17f15f4c5546176d25a476628729c270dc55
Eustegney <eustegney@yandex.ru>
2020-09-29 23:39:33 +0700

    module finished

--- class_gamepad.py
+++ class_gamepad.py
@@ -1,24 +1,47 @@
 import pygame
 
+
 class Gamepad:
+    """Class to create object for single Joystick
+
+    Automatically initializes during initialisation. User can initialize or un-initialize it via subs 'init' and 'quit'
+    Multiple instances of Gamepad must work with single instance of JoystickEvents class.
+    Usage:
+    1. Create 1 JoystickEvents object and as many Gamepad objects as you need
+        je = JoystickEvents()
+        j1 = Gamepad(0)
+        j2 = Gamepad(1)
+    2. Add to mainloop:
+         if j1.is_new_events(je):
+            event = j1.get_event(je)
+
+    """
 
     def __init__(self, gamepad_id):
+        """ Connect Gamepad to physical device if available
+
+        Initialize Joystick module
+        and check if device(id) is present
+        :param gamepad_id: system id of joystick
+
+        """
 
         self.exist = False
         self.axe_count = 0
         self.button_count = 0
         self.id = gamepad_id
+        self.is_events = False
 
         if not pygame.joystick.get_init():
             pygame.joystick.init()
 
         gamepad_count = pygame.joystick.get_count()
 
-        if gamepad_count < 1: # gamepad not connected
+        if gamepad_count < 1:  # gamepad not connected
             print('gamepad not connected')
-        elif type(gamepad_id) != int: # id is not 'int' class
+        elif type(gamepad_id) != int:  # id is not 'int' class
             print('id must be int')
-        elif gamepad_id < 0 or gamepad_count <= gamepad_id: # id out of range
+        elif gamepad_id < 0 or gamepad_count <= gamepad_id:  # id out of range
             print('wrong id', gamepad_id)
         else:
             print('gamepad', gamepad_id, 'found')
@@ -31,54 +54,95 @@ class Gamepad:
             self.axe_count = self.Gamepad.get_numaxes()
 
     def init(self):
+        """Initialize current instance if present"""
+
         if self.exist:
             self.Gamepad.init()
         else:
             print('this gamepad not exists')
 
     def quit(self):
+        """Un-initialize current instance if present"""
+
         if self.exist:
             self.Gamepad.quit()
         else:
             print('this gamepad not exists')
 
     def get_button_count(self):
+        """Returns number of buttons of current device
+
+        :return integer
+
+        """
+
         return self.button_count
 
     def get_axe_count(self):
+        """Returns number of axes of current device
+
+        :return integer
+
+        """
         return self.axe_count
 
-    def get_event(self, joy_event_list):
+    def get_event(self, joystick_events_instance):
+        """Return event for current device, and delete it from event_list
 
-            for event in joy_event_list:
-                if event[1] == 10:  #JOYBUTTONDOWN
-                    event_return = [True, event[1], event[2]]
-                    return event_return
-                elif event[1] == 11:  #JOYBUTTONUP
-                    event_return = [True, event[1], event[2]]
-                    return event_return
-                elif event[1] == 7:  #JOYAXISMOTION
-                    event_return = [True, event[1], event[2], event[3]]
-                    return event_return
-                else:
-                    event_return = [False]
-                    return event_return
+        Event is a list:
+            for button [event, button number]
+                event:  10 - button down
+                        11 - button up
+            for axe [event, axis number, value]
+                event: 7 - axis move
+                value: float from -1 to 1 (0 - center)
+
+        :returns list
+
+        """
+
+        if len(joystick_events_instance.event_list[self.id]) > 0:
+            return joystick_events_instance.event_list[self.id].pop()
+
+    def is_new_events(self, joystick_events_instance):
+        """Update events
+
+         and put to event list if present
+
+        :returns True if new events found, else returns False
+
+        """
+
+        joystick_events_instance.append_events_to_event_list()
+
+        if len(joystick_events_instance.event_list[self.id]) > 0:
+            return True
+        else:
+            return False
+
 
 class JoystickEvents:
+    """Class for joystick events
+
+    You should use only one instance of this class
+
+    """
 
     def __init__(self):
+        pygame.init()
         self.event_list = [[], []]
 
     def append_events_to_event_list(self):
         """Check joystick events and put to self.event_list
 
-        also clean current API event list
+        Also clean current API event list.
+        Ignoring and delete all other events (mouse, keyboard, midi)
 
         """
 
         if pygame.event.peek():
             for event in pygame.event.get():
-                if event.type == 10:  #JOYBUTTONDOWN
+                if event.type == 10:  # JOYBUTTONDOWN
                     append_element = [event.type, event.button]
                     self.event_list[event.joy].append(append_element)
                 elif event.type == 11:  # JOYBUTTONUP
@@ -91,7 +155,10 @@ class JoystickEvents:
     def get_joy_event_list(self, joy):
         """Move all events for custom joystick to list in return
 
+        :returns list
+
         """
+
         joy_event_list = []
         index = 0
         for event in self.event_list[joy]:
@@ -103,24 +170,18 @@ class JoystickEvents:
 
 if __name__ == '__main__':
 
-    gamepad1_id = 0
-    gamepad2_id = 1
-
-    pygame.init()
     ev = JoystickEvents()
-    j0 = Gamepad(gamepad1_id)
-    j1 = Gamepad(gamepad2_id)
-    j0.init()
-    j1.init()
+    j0 = Gamepad(0)
+    j1 = Gamepad(1)
+
     a = True
     timer = pygame.time.Clock()
 
     while a:
         timer.tick(100)
 
-        ev.append_events_to_event_list()
+        if j0.is_new_events(ev):
+            print('Gamepad 0: ', j0.get_event(ev))
 
-        for dev_id in range(pygame.joystick.get_count()):
-            for index in range(len(ev.event_list[dev_id])):
-                aa = ev.event_list[dev_id].pop()
-                print(aa)
+        if j1.is_new_events(ev):
+            print('Gamepad 1: ', j1.get_event(ev))
